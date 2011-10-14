3b72d935c7918f8c176251744effea29947d9f89
uemit.seren <uemit.seren@gmi.oeaw.ac.at>
2011-10-14 22:43:48 +0200

    New Commands registered

--- frontend/src/com/gmi/rnaseqwebapp/client/gin/RNASeqClientActionHandlerRegistry.java
+++ frontend/src/com/gmi/rnaseqwebapp/client/gin/RNASeqClientActionHandlerRegistry.java
@@ -1,5 +1,8 @@
 package com.gmi.rnaseqwebapp.client.gin;
 
+import com.gmi.rnaseqwebapp.client.command.GetGWASDataActionHandler;
+import com.gmi.rnaseqwebapp.client.command.GetPhenotypeDataActionHandler;
+import com.gmi.rnaseqwebapp.client.command.GetPhenotypeMotionChartDataActionHandler;
 import com.gmi.rnaseqwebapp.client.command.GetPhenotypersActionHandler;
 import com.google.inject.Inject;
 import com.gwtplatform.dispatch.client.actionhandler.DefaultClientActionHandlerRegistry;
@@ -8,8 +11,14 @@ public class RNASeqClientActionHandlerRegistry extends
 		DefaultClientActionHandlerRegistry {
 	
 	@Inject
-	public RNASeqClientActionHandlerRegistry(GetPhenotypersActionHandler getPhenotypesActionHandler) {
+	public RNASeqClientActionHandlerRegistry(GetPhenotypersActionHandler getPhenotypesActionHandler,
+											GetPhenotypeDataActionHandler getPhenotypeActionHandler,
+											GetPhenotypeMotionChartDataActionHandler getPhenotypeMotionChartActionHandler,
+											GetGWASDataActionHandler getGWASDataActionHandler) {
 		register(getPhenotypesActionHandler);
+		register(getPhenotypeActionHandler);
+		register(getPhenotypeMotionChartActionHandler);
+		register(getGWASDataActionHandler);
 	}
 	
 
