c338248e87f023f78ddc5d3376ec674a47a515b1
uemit.seren <uemit.seren@gmi.oeaw.ac.at>
2011-10-14 22:41:07 +0200

    Environment fields

--- frontend/src/com/gmi/rnaseqwebapp/client/dto/Environment.java
+++ frontend/src/com/gmi/rnaseqwebapp/client/dto/Environment.java
@@ -1,5 +1,26 @@
 package com.gmi.rnaseqwebapp.client.dto;
 
-public class Environment extends BaseModel {
+import java.util.List;
 
+public class Environment extends BaseModel {
+	
+	public enum TYPES {T16C,T10C};
+	
+	String name;
+	String phenotype;
+	
+	List<Dataset> datasets;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getPhenotype() {
+		return phenotype;
+	}
+	
+	
+	public List<Dataset> getDatasets() {
+		return datasets;
+	}
 }
