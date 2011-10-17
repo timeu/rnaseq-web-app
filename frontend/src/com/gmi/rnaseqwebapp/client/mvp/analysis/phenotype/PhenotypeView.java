f5b94b010bf88988e7f840c0442888ae1ddf644a
uemit.seren <uemit.seren@gmi.oeaw.ac.at>
2011-10-17 17:15:35 +0200

    Design Changes for top navigation

--- frontend/src/com/gmi/rnaseqwebapp/client/mvp/analysis/phenotype/PhenotypeView.java
+++ frontend/src/com/gmi/rnaseqwebapp/client/mvp/analysis/phenotype/PhenotypeView.java
@@ -5,9 +5,13 @@ import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 import com.gmi.rnaseqwebapp.client.NameTokens;
 import com.gmi.rnaseqwebapp.client.dto.Phenotype;
+import com.gmi.rnaseqwebapp.client.resources.MyResources;
 import com.gmi.rnaseqwebapp.client.ui.SlidingPanel;
+import com.google.gwt.dom.client.LIElement;
+import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
@@ -22,15 +26,24 @@ public class PhenotypeView extends ViewImpl implements
 	}
 	
 	@UiField InlineLabel phenotype_info;
+	@UiField InlineLabel phenotype_name;
+	
 	@UiField SlidingPanel container;
 	@UiField InlineHyperlink overviewLink;
 	@UiField InlineHyperlink T16Link;
 	@UiField InlineHyperlink T10Link;
+	@UiField LIElement overview_item;
+	@UiField LIElement t16_item;
+	@UiField LIElement t10_item;
+	@UiField(provided=true)final MyResources mainRes;
+	
+	public enum NAV_ITEMS {Overview,T16C,T10C};
 	
 	private final PlaceManager placeManager;
 
 	@Inject
-	public PhenotypeView(final Binder binder, final PlaceManager placeManager) {
+	public PhenotypeView(final Binder binder, final PlaceManager placeManager, final MyResources resources) {
+		this.mainRes = resources;
 		widget = binder.createAndBindUi(this);
 		this.placeManager = placeManager;
 	}
@@ -42,22 +55,41 @@ public class PhenotypeView extends ViewImpl implements
 
 	@Override
 	public void showPhenotypeInfo(Phenotype phenotype) {
-		String info = phenotype.getName() + " (Chr: "+phenotype.getChr() + " | " + phenotype.getStart() + " - " + phenotype.getEnd() + ")";
+		String info = "Chr: "+phenotype.getChr() + " | " + phenotype.getStart() + " - " + phenotype.getEnd();
 		phenotype_info.setText(info);
-		overviewLink.setTargetHistoryToken(placeManager.buildHistoryToken(new PlaceRequest(NameTokens.phenotypepage).with("id", phenotype.getName())));
-		T16Link.setTargetHistoryToken(placeManager.buildHistoryToken(placeManager.getCurrentPlaceRequest().with("env", "T16C")));
-		T10Link.setTargetHistoryToken(placeManager.buildHistoryToken(placeManager.getCurrentPlaceRequest().with("env", "T10C")));
+		phenotype_name.setText(phenotype.getName()+": ");
+		PlaceRequest request = new PlaceRequest(NameTokens.phenotypepage).with("id", phenotype.getName());
+		overviewLink.setTargetHistoryToken(placeManager.buildHistoryToken(request));
+		T16Link.setTargetHistoryToken(placeManager.buildHistoryToken(request.with("env", NAV_ITEMS.T16C.toString())));
+		T10Link.setTargetHistoryToken(placeManager.buildHistoryToken(request.with("env", NAV_ITEMS.T10C.toString())));
 	}
 
 	@Override
 	public void setInSlot(Object slot, Widget content) {
 		if (slot == PhenotypePresenter.TYPE_SetMainContent) {
-			//container.clear();
 			container.setWidget(content);
 		}
 		else {
 			super.setInSlot(slot, content);
 		}
-		
+	}
+	
+	@Override
+	public void setActiveLink(NAV_ITEMS item) {
+		String selected_class_name = mainRes.style().indicator_small_icon();
+		overview_item.removeClassName(selected_class_name);
+		t16_item.removeClassName(mainRes.style().indicator_small_icon());
+		t10_item.removeClassName(mainRes.style().indicator_small_icon());
+		switch (item) {
+			case Overview:
+				overview_item.addClassName(selected_class_name);
+				break;
+			case T10C:
+				t10_item.addClassName(selected_class_name);
+				break;
+			case T16C:
+				t16_item.addClassName(selected_class_name);
+				break;
+		}
 	}
 }
