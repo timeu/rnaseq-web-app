e8b32f3e005dc47066688d0f67482d9e8ab20902
Uemit Seren <uemit.seren@gmail.com>
2012-01-18 16:06:03 +0100

    ResizableCharts added

---
+++ frontend/src/main/java/com/gmi/rnaseqwebapp/client/ui/ResizeableBarChart.java
@@ -0,0 +1,34 @@
+package com.gmi.rnaseqwebapp.client.ui;
+
+import com.google.gwt.user.client.ui.RequiresResize;
+import com.google.gwt.visualization.client.AbstractDataTable;
+import com.google.gwt.visualization.client.visualizations.corechart.BarChart;
+import com.google.gwt.visualization.client.visualizations.corechart.Options;
+
+public class ResizeableBarChart extends BarChart implements RequiresResize {
+
+	
+	protected Options options;
+	protected AbstractDataTable data;
+	
+	public ResizeableBarChart(AbstractDataTable data, Options options) {
+		super(data, options);
+		this.data = data;
+		this.options = options;
+		
+	}
+
+	@Override
+	public void onResize() {
+		options.setWidth(getParent().getOffsetWidth());
+		options.setHeight(getParent().getOffsetHeight());
+		draw(data, options);
+	}
+	
+	public void draw2(AbstractDataTable data,Options options) {
+		this.options = options;
+		this.data = data;
+		draw(data,options);
+	}
+
+}
